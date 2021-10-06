2769171f6c09f6459ca956d5ea5f2b0432501547
Gabriel Oliveira <gogvale2011@gmail.com>
2021-10-06 18:44:38 -0500

    Migrate to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_06_180218) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "packets", force: :cascade do |t|
     t.string "name"
     t.float "weight"
